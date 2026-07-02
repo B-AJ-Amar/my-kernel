0e7c4e12aa2ff3052267a903b13d3a35aea8908f
Amar A.J Bouakaz <amarbouakaz91@gmail.com>
2026-07-02 01:05:10 +0100

    feat(boot): load kernel

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,21 +4,39 @@
 CODE_SEG equ gdt_code_seg - gdt_start; should be 0x8 (selector)
 DATA_SEG equ gdt_data_seg - gdt_start; 0x10
 
+KERNEL_LOAD_ADDR equ 0x1000
+KERNEL_ADDR_PM   equ 0x10000000 ;protected mod address
+
 start:
+    call load_kernel
     cli
     lgdt [gdt_decriptor]
     mov eax, cr0
     or eax, 1
     mov cr0, eax
     jmp CODE_SEG:start_protected_mode
+
+
+; read from disk https://en.wikipedia.org/wiki/INT_13H
+load_kernel:
+    mov bx, KERNEL_LOAD_ADDR;load to addr
+    mov ch, 0x00;cylinder
+    mov dh, 0x00;head
+    mov cl, 0x02;sector
+    mov dl, 0x80; first disk
+    mov ah, 0x02;write mode
+    ;! dont remove token i will use it in the build time
+    mov al, 1 ; <SECTORS_LEN>  number of sectors (i will upt this in build time)
+    int 0x13
+    
     
 gdt_start:
     gdt_null:
         dd 0x0
         dd 0x0
     gdt_code_seg:
-        dw 0xffff     ;limit[0->15]
-        dw 0x0     ;base[0->15]
+        dw 0xffff    ;limit[0->15]
+        dw 0x0       ;base[0->15]
         db 0x0       ;base[16->23]
         db 10011010b ;access flags
         db 11001111b ;flags + limit[16->19]
@@ -66,6 +84,8 @@ start_protected_mode:
         add ebx, 2
         jmp print
     print_end:
+
+    jmp CODE_SEG:KERNEL_ADDR_PM
     hlt
 
 
