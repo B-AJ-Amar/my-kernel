a5bdd5ece2daa93bb1d0a6790a8a3b96aa150edc
Amar A.J Bouakaz <amarbouakaz91@gmail.com>
2026-07-04 21:48:29 +0100

    fix: kernel size move

--- scripts/kernel-size.sh
+++ scripts/kernel-size.sh
@@ -9,8 +9,10 @@ fi
 
 SECTORS=$(((FILE_SIZE + 511) / 512))
 
+KERNEL_SIZE_D=$((FILE_SIZE / 4))
 
-sed -i "s/[0-9]\+[[:space:]]*;[[:space:]]*<KERNEL_SIZE>/$FILE_SIZE ; <KERNEL_SIZE>/" boot/boot.asm
+
+sed -i "s/[0-9]\+[[:space:]]*;[[:space:]]*<KERNEL_SIZE>/$KERNEL_SIZE_D ; <KERNEL_SIZE>/" boot/boot.asm
 
 sed -i "s/[0-9]\+[[:space:]]*;[[:space:]]*<SECTORS_LEN>/$SECTORS ; <SECTORS_LEN>/" boot/boot.asm
 echo $SECTORS
